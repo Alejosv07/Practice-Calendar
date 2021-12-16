c94ca724a99a48b53fead92a9619d89bb0f18acf
Alejosv07 <balrking07@gmail.com>
2021-12-15 18:36:40 -0600

    change styles

--- js/logica.js
+++ js/logica.js
@@ -6,17 +6,19 @@ const darkMode = document.getElementById("darkMode");
 
 
 
-let month = new Date().getMonth();
+let month = new Date().getMonth() + 1;
 let year = new Date().getFullYear();
 let months = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"
 ];
-let weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "SaturDay", "Sunday"];
-let modeDark = false;
+let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let modeDark = true;
+
+
 
 //constructor init
 const init = () => {
-    calendario__month.innerText = months[month - 1] + " " + year;
+    calendario__month.innerText = months[month - 2] + " " + year;
     btnPreview.addEventListener("click", () => { chageDate(-1) });
     btnNext.addEventListener("click", () => { chageDate(1) });
     darkMode.addEventListener("click", () => {
@@ -38,16 +40,26 @@ function chageDate(btn) {
     let template = document.createElement("template");
     let quantityDay = new Date(year, month, 0).getDate() + 1;
 
+
+    let today = new Date(new Date().getMonth() + 1 + "-" + new Date().getDate() + "-" + new Date().getFullYear());
     let event;
     let options = { weekday: 'long' };
 
+    let headerDay = "";
     for (let index = 1; index < quantityDay; index++) {
-        event = new Date(Date.parse(year + "-" + month + "-" + index));
+        event = new Date(month + "-" + index + "-" + year);
+
+        //If is today back special color
+        if (event.getTime() == today.getTime()) {
+            headerDay = `<div class="calendario__celda active-day">`;
+        } else {
+            headerDay = `<div class="calendario__celda">`;
+        }
         template.innerHTML += `
-        <div class="calendario__celda">
+        ${headerDay}
         <!-- day -->
-        <h3 class="calendario__celda__day">${index}</h3>
-        <h4 class="calendario__celda__week__day">${event.toLocaleDateString('en-US', options)}</h4>
+        <h3 class="calendario__celda__day">${event.getDate()}</h3>
+        <h4 class="calendario__celda__week__day">${weekDays[event.getDay()]}</h4>
         <!-- end day -->
         <!-- calendario footer -->
         <div class="calendario__celda__footer">
@@ -63,22 +75,20 @@ function chageDate(btn) {
         </div>
         <!-- end calendario footer -->
         </div>`;
-        console.log("Dia " + index);
-        console.log("mes " + month);
-        console.log("ano " + year);
-        console.log(event);
     }
     calendario.innerHTML = template.innerHTML;
 }
 
 const modeDarkF = () => {
     if (modeDark) {
-        document.documentElement.style.setProperty("--color--p", "black");
-        document.documentElement.style.setProperty("--color--s", "white");
+        document.documentElement.style.setProperty("--color-oscuro", "white");
+        document.documentElement.style.setProperty("--color-fondo", "black");
+        document.documentElement.style.setProperty("--color-claro", "#1F1E24");
         modeDark = false;
     } else {
-        document.documentElement.style.setProperty("--color--p", "white");
-        document.documentElement.style.setProperty("--color--s", "black");
+        document.documentElement.style.setProperty("--color-oscuro", "black");
+        document.documentElement.style.setProperty("--color-fondo", "#ededed");
+        document.documentElement.style.setProperty("--color-claro", "white");
         modeDark = true;
     }
 }
